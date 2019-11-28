f517972f6683b8100ea8466cec3632c18a3d424e
OZKAR AGOZ <52090605+OzkarAgoz@users.noreply.github.com>
2019-11-28 13:48:13 -0600

    markers

---
+++ Leaflet_Cluster_ExampleEXCELLENTcopy/maps/leaf-demo.js
@@ -0,0 +1,63 @@
+// See post: http://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
+
+
+var map = L.map( 'map', {
+  center: [29.0, -93.0],
+  // minZoom: 2,
+  zoom: 5
+});
+
+var ourmap = L.tileLayer("https://api.mapbox.com/styles/v1/ozkar/ck2z6q6h72eyd1cmtm084dkrh/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1Ijoib3prYXIiLCJhIjoiY2sydjVkYmFyMDB6MjNobzN5d3h0YWo0dyJ9.bmh0jz1RRr2djPQTiM0T1Q").addTo( map );
+
+// L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//  subdomains: ['a','b','c']
+// }).addTo( map );
+
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+});
+
+var markerClusters = L.markerClusterGroup();
+
+for ( var i = 0; i < markers.length; ++i )
+{
+  var popup = markers[i].name +
+  '<br/>' + markers[i].category +
+  '<br/><b>rating:</b> ' + Math.round( markers[i].rating * 1 ) + ' m' +
+  '<br/><b>addr:</b> ' + markers[i].address;
+
+  var m = L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+                  .bindPopup( popup );
+
+  markerClusters.addLayer( m );
+}
+for ( var i = 0; i < pizza.length; ++i )
+{
+  var popup = pizza[i].name +
+  '<br/>' + pizza[i].category +
+  '<br/><b>rating:</b> ' + Math.round( pizza[i].rating * 1 ) + ' m' +
+  '<br/><b>addr:</b> ' + pizza[i].address;
+
+  var p = L.marker( [pizza[i].lat, pizza[i].lng], {icon: myIcon} )
+                  .bindPopup( popup );
+
+  markerClusters.addLayer( p );
+}
+var baseMaps = {
+  "ourmap": ourmap
+};
+var overlayMaps = {
+  "Markers": m,
+  "Pizza": p
+};
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+map.addLayer( markerClusters );
